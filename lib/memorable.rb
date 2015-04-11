680a957c63e7cf205b74868020702c0edf8353e7
zvkemp <zvkemp@gmail.com>
2015-04-11 12:41:08 -0700

    build api

--- lib/memorable.rb
+++ lib/memorable.rb
@@ -1,6 +1,19 @@
 require 'memorable/version'
 module Memorable
   module ClassMethods
+    def memoize(sym, &block)
+      instance_eval do
+        if block_given?
+          define_method(sym) do |*args|
+            memoize(sym) { instance_exec(*args, &block) }
+          end
+        else
+          old_method = :"_nm_#{sym}"
+          alias_method old_method, sym
+          memoize(sym) {|*args| send(old_method, *args) }
+        end
+      end
+    end
   end
 
   def self.included(base)
@@ -9,10 +22,18 @@ module Memorable
 
   private
 
-  def memoize
-    _memoized.fetch(caller_locations.first.label) do |m|
+  def memoize(sym = nil)
+    sym ||= caller_locations.first.label.to_sym
+    _memoized.fetch(sym) do |m|
       _memoized[m] = yield
     end
+  end
+
+  # Execute the block and store the value even if it already exists.
+  # Useful for writer methods.
+  def memoize!(sym = nil)
+    sym ||= caller_locations.first.label.to_sym
+    _memoized[sym] = yield
   end
 
   def _memoized
--- spec/lib/memorable_spec.rb
+++ spec/lib/memorable_spec.rb
@@ -1,6 +1,97 @@
 require_relative '../spec_helper'
-require 'pry'
+require 'rr'
+require 'securerandom'
 
 describe Memorable do
+  class DumDum
+    include Memorable
+
+    def uuid
+      memoize { SecureRandom.uuid }
+    end
+
+    def uuid=(str)
+      memoize!(:uuid) { str }
+    end
+
+    def guid
+      memoize(:some_key) { SecureRandom.guid }
+    end
+  end
+
   specify { Memorable::VERSION.wont_be_nil }
+
+  let(:instance) { DumDum.new }
+
+  describe 'lazy readers' do
+    it 'is instance-idempotent' do
+      instance.uuid.must_equal instance.uuid
+    end
+
+    it 'is globally unique' do
+      DumDum.new.uuid.wont_equal instance.uuid
+    end
+  end
+
+  describe 'writers' do
+    let(:uuid) { SecureRandom.uuid }
+    
+    it 'overwrites the existing memoized value' do
+      original = instance.uuid
+      instance.uuid = uuid
+      instance.uuid.must_equal uuid
+      original.wont_equal instance.uuid
+    end
+  end
+
+  describe 'class methods' do
+    class ClassyDumDum
+      include Memorable
+      memoize(:uuid) { SecureRandom.uuid }
+    end
+
+    let(:instance) { ClassyDumDum.new }
+
+    describe 'memoize with proc' do
+      specify do
+        instance.uuid.wont_be_nil
+        instance.uuid.must_equal instance.uuid
+      end
+    end
+
+    describe 'a proc with instance methods' do
+      class ClassyDumDum
+        memoize(:modified_uuid) { modify(SecureRandom.uuid) }
+
+        def modify(obj)
+          "modified-#{obj}"
+        end
+      end
+
+      specify do
+        instance.modified_uuid.must_include 'modified-'
+        instance.modified_uuid.must_equal instance.modified_uuid
+        instance.modified_uuid.wont_equal instance.uuid
+      end
+    end
+
+    describe 'memoize an existing method' do
+      class ClassyDumDum
+        def my_unique_method
+          concat('unique', SecureRandom.uuid)
+        end
+
+        def concat(*strs)
+          strs.join('-')
+        end
+
+        memoize :my_unique_method
+      end
+
+      specify do
+        instance.my_unique_method.must_equal instance.my_unique_method
+        instance.my_unique_method.must_include 'unique-'
+      end
+    end
+  end
 end
